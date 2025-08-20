3426d89e9272ec04c631476d5042cfe5e9bb143a
luis felipe <lfdelahozfontalvo@gmail.com>
2025-08-20 11:09:44 -0500

    perfil usuario

--- Modulos/Login/logica/auth_routesL.py
+++ Modulos/Login/logica/auth_routesL.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Form
 from fastapi.responses import HTMLResponse
-from Modulos.Login.logica.auth_consultL import login_user, register_user
+from Modulos.Login.logica.auth_consultL import login_user, register_user, update_user, view_user
 
 router = APIRouter()
 
@@ -14,7 +14,7 @@ def loger():
 def login(username: str = Form(...), password: str = Form(...)):
     user = login_user(username, password)
     if user:
-        return HTMLResponse(content=f"<script>alert('Bienvenid@ a nuestra tienda {username}'); window.location.href='/';</script>")
+        return HTMLResponse(content=f"<script>alert('Bienvenid@ a nuestra tienda {username}'); window.location.href='/updateP';</script>")
     else:
         return HTMLResponse(content="<script>alert('Usuario o contraseña incorrectos'); window.location.href='/';</script>")
 
@@ -25,6 +25,19 @@ def register():
 @router.post("/register", response_class=HTMLResponse)
 def register(id: int = Form(...), username: str = Form(...), phone: int = Form(...), email: str = Form(...), password: str = Form(...)):
     if register_user(id, username, phone, email, password):
-        return HTMLResponse(content="<script>alert('Usuario registrado exitosamente'); window.location.href='/register';</script>")
+        return HTMLResponse(content="<script>alert('Usuario registrado exitosamente'); window.location.href='/login';</script>")
     else:
         return HTMLResponse(content="<script>alert('Error al registrar usuario'); window.location.href='/register';</script>")    
+    
+@router.get("/updateP", response_class=HTMLResponse)
+def updateP():
+     with open("Modulos/Login/vista/perfil.html", "r", encoding= "utf-8") as f:
+        return HTMLResponse(content=f.read())
+     
+@router.post("/updateP", response_class=HTMLResponse)
+def updateP(id: int = Form(...), username: str = Form(...), phone: int = Form(...), email: str = Form(...), password: str = Form(...)):
+    # colocaremos la funcion def view_user para obtener los datos del usuario y mostrarlos en el formulario segun el id con el que se inicio sesion
+    if update_user(id, username, phone, email, password):
+        return HTMLResponse(content="<script>alert('Usuario actualizado exitosamente'); window.location.href='/updateP';</script>")
+    else:
+        return HTMLResponse(content="<script>alert('Error al actualizar usuario'); window.location.href='/updateP';</script>")
