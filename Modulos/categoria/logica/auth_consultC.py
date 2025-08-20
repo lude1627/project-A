732429f01b378e3335b03fd55964eccb4c0f217f
luis felipe <lfdelahozfontalvo@gmail.com>
2025-08-20 17:28:14 -0500

    aun falta el perfil

---
+++ Modulos/categoria/logica/auth_consultC.py
@@ -0,0 +1,26 @@
+from Modulos.db import conexion
+
+def create_cat(id: int, name: str):
+    cursor = conexion.cursor()
+    query = "INSERT INTO categorias (Cat_id, Cat_name) VALUES (%s, %s)"
+    cursor.execute(query, (id, name))
+    conexion.commit()
+    cursor.close()
+    return True
+
+def view_cat():
+    cursor = conexion.cursor()
+    query = "SELECT * FROM categorias"
+    cursor.execute(query)
+    categories = cursor.fetchall()
+    cursor.close()
+    return categories
+
+
+def delete_cat(id: int):
+    cursor = conexion.cursor()
+    query = "DELETE FROM categorias WHERE Cat_id = %s"
+    cursor.execute(query, (id,))
+    conexion.commit()
+    cursor.close()
+    return True
