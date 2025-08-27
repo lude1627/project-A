636588b733b91b8d2d0eaf630e5a8fd3a698aab5
Raul Osorio <rosoriocastro2002@gmail.com>
2025-08-27 15:50:57 -0500

    categoria "fix"

--- Modulos/categoria/logica/auth_consultC.py
+++ Modulos/categoria/logica/auth_consultC.py
@@ -1,22 +1,23 @@
-from Modulos.db import execute_query,conexion
+from Modulos.db import execute_query
 
 def create_cat(id: int, name: str):
     query = "INSERT INTO categorias (Cat_id, Cat_name) VALUES (%s, %s)"
     try:
-        execute_query(query, (id, name),commit=True)
-        return True
+        categoria = execute_query(query, (id, name),commit=True)
+        return categoria
     except Exception as e:
         print("Error al crear una categoria: {e}")
         return False
     
-def view_cat():
-    cursor = conexion.cursor(dictionary=True)
-    query = "SELECT * FROM categorias"
-    cursor.execute(query)
-    categories = cursor.fetchall()
-    cursor.close()
-    return categories
+def all_categories():
+   
+    query="SELECT * FROM categorias"
+    try:
+        categorias = execute_query(query,fetchall=True)  
+        return categorias
+    except Exception as e:
 
+        print("Error al mostrar las categorias : {e}")
 def delete_cat(id: int):
     query = "DELETE FROM categorias WHERE Cat_id = %s"
     try:
--- Modulos/categoria/logica/auth_routesC.py
+++ Modulos/categoria/logica/auth_routesC.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Form
 from fastapi.responses import HTMLResponse, JSONResponse
-from Modulos.categoria.logica.auth_consultC import create_cat, view_cat, delete_cat
+from Modulos.categoria.logica.auth_consultC import create_cat,all_categories , delete_cat
 
 router = APIRouter()
 
@@ -9,18 +9,27 @@ def categories():
     with open("Modulos/categoria/vista/categorias.html", "r", encoding="utf-8") as f:
         return HTMLResponse(content=f.read())
     
+    
+@router.get("/view_category/data")
+def get_category():
+    categoria = all_categories()
+    category_json = [
+        {
+            "Cat_id": cat[0],
+            "Cat_name": cat[1]
+        }
+        for cat in categoria
+    ]
+    return JSONResponse(content=category_json)
+
+
+    
 @router.post("/categorySave", response_class=HTMLResponse)
 def create_category(id: int = Form(...), name: str = Form(...)):
     if create_cat(id, name):
         return HTMLResponse(content="<script>alert('Categoría creada exitosamente'); window.location.href='/category';</script>")
     else:
         return HTMLResponse(content="<script>alert('Error al crear categoría'); window.location.href='/category';</script>")    
-
-#buscando opcion de llamar los datos de la tabla categorias.
-@router.get("/List_Category")
-def listar_categorias():
-    categories = view_cat()
-    return JSONResponse(content=categories)
 
 @router.get("/category/delete", response_class=HTMLResponse)
 def delete_category(id: int):
