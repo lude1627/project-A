cd868758c2b2c6bec8a9e198b83c3e7561dbbc5d
Raul Osorio <rosoriocastro2002@gmail.com>
2025-08-27 10:53:26 -0500

    update

--- Modulos/carrito_compras/logica/auth_consultasCar.py
+++ Modulos/carrito_compras/logica/auth_consultasCar.py
@@ -1,22 +1,24 @@
-from Modulos.db import  conexion
+from Modulos.db import  execute_query
+
 
 def get_items():
-    cursor = conexion.cursor()
     query = """ 
-        SELECT 
-            c.Car_id,
-            u.User_id,
-            u.User_name,
-            p.Product_id,
-            p.Product_name,
-            c.Car_Cantidad,
-            p.Product_price AS precio,
-            (c.Car_Cantidad * p.Product_price) AS subtotal
-        FROM carrito c
-        JOIN productos p ON c.Product_id = p.Product_id
-        JOIN usuario u ON c.User_id = u.User_id;
+        SELECT c.Car_id, u.User_id, u.User_name, p.Product_id, p.Product_name, c.Car_Cantidad, p.Product_price AS precio, (c.Car_Cantidad * p.Product_price) AS subtotal
+        FROM carrito c JOIN productos p ON c.Product_id = p.Product_id JOIN usuario u ON c.User_id = u.User_id;
         """
-    cursor.execute(query)
-    carrito = cursor.fetchall()
-    cursor.close()
-    return carrito
+    try:
+            carrito = execute_query(query, fetchall=True)
+            return carrito
+    except Exception as e:
+            print("Error al mostrar compras: {e}")
+            return False
+    
+# def eliminar_producto_db(id: int) -> bool:
+#     try:
+#         query = "DELETE FROM productos WHERE id = %s" 
+#         execute_query(query,(id),commit=True)
+#         return 
+#     except Exception as e:
+#         print("Error al eliminar: {e}")
+#         return False
+
